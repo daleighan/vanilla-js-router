cd19bf9b4c935d9991fe262471ba9d5edde8d2ed
David Alexander Leigh <daleighan@gmail.com>
2019-10-12 21:48:19 -0700

    Added a route to navigate to other routes

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-function Router(container, routes) {
+function Router(container, routes, errorHTML) {
   this.router = document.createElement('div');
   this.routes = routes;
-  console.log('Router Initialized with Routes: ', Object.keys(this.routes).join(', '));
-  console.log('Current Path: ', window.location.pathname)
+  console.log(
+    'Router Initialized with Routes: ',
+    Object.keys(this.routes).join(', '),
+  );
+  console.log('Current Path: ', window.location.pathname);
   document.getElementById(container).appendChild(this.router);
-  this.router.innerHTML = this.routes[window.location.pathname.replace(/\/$/, "")] || '<div>Not Found</div>';
+  if (this.routes[window.location.pathname]) {
+    this.router.innerHTML = this.routes[window.location.pathname];
+  } else {
+    this.router.innerHTML = errorHTML ? erroHTML : '<div>Not Found</div>';
+  }
+  this.goTo = function(route) {
+    window.history.pushState({}, route, `${window.location.origin}/${route}/`);
+    this.router.innerHTML = routes[window.location.pathname];
+    if (this.routes[window.location.pathname]) {
+      this.router.innerHTML = this.routes[window.location.pathname];
+    } else {
+      this.router.innerHTML = errorHTML ? erroHTML : '<div>Not Found</div>';
+    }
+  };
+  window.router = this;
 }
 
 module.exports = Router;
