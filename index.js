83e8db6766ec40cf5eb56f1f7e694b82b9f4e25b
David Alexander Leigh <daleighan@gmail.com>
2019-10-12 21:24:40 -0700

    The router will now render the requested route

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 function Router(container, routes) {
   this.router = document.createElement('div');
   this.routes = routes;
+  console.log('Router Initialized with Routes: ', Object.keys(this.routes).join(', '));
+  console.log('Current Path: ', window.location.pathname)
+  document.getElementById(container).appendChild(this.router);
+  this.router.innerHTML = this.routes[window.location.pathname.replace(/\/$/, "")] || '<div>Not Found</div>';
 }
 
 module.exports = Router;
