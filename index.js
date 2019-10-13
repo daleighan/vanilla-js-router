0275db0a085bcce7b70721e99d2d4a1da749cbbf
David Alexander Leigh <daleighan@gmail.com>
2019-10-13 00:25:41 -0700

    Adding prettier configuration and pre-commit hook

--- index.js
+++ index.js
@@ -1,92 +1,92 @@
 function Router(container, routes, options) {
   function _appendComponent(elementName, element, anchor, opts) {
     if (opts && opts.clearAnchor) {
-      anchor.innerHTML = '';
+      anchor.innerHTML = ''
     }
-    this[elementName] = element;
+    this[elementName] = element
     if (typeof element === 'string') {
-      var createdElement = document.createElement('div');
-      createdElement.innerHTML = element;
-      anchor.appendChild(createdElement);
+      var createdElement = document.createElement('div')
+      createdElement.innerHTML = element
+      anchor.appendChild(createdElement)
     } else {
-      anchor.appendChild(element);
+      anchor.appendChild(element)
     }
   }
 
   function _goTo(route) {
-    window.history.pushState({}, route, window.location.origin + route);
-    var pathname = window.location.pathname;
+    window.history.pushState({}, route, window.location.origin + route)
+    var pathname = window.location.pathname
     if (this.routes[pathname]) {
       _appendComponent.call(
         this,
         'currentView',
         routes[pathname],
         _routerContainer,
-        {clearAnchor: true},
-      );
+        { clearAnchor: true }
+      )
       if (options.debug) {
         console.log(
           '%cNavigated to: ' + route,
-          'color: green; font-size: 14px;',
-        );
+          'color: green; font-size: 14px;'
+        )
       }
-      _replaceLinks.call(this, this, _routerContainer);
+      _replaceLinks.call(this, this, _routerContainer)
     } else {
       _appendComponent.call(
         this,
         'currentView',
         this.errorHTML,
         _routerContainer,
-        {clearAnchor: true},
-      );
+        { clearAnchor: true }
+      )
       if (options.debug) {
-        console.error('Route not found: ' + route);
+        console.error('Route not found: ' + route)
       }
     }
   }
 
-  this.goTo = _goTo.bind(this);
+  this.goTo = _goTo.bind(this)
 
   function _replaceLinks(ctx, containerForReplacement) {
     containerForReplacement
       .querySelectorAll('.router-link')
       .forEach(function(link) {
-        link.onclick = _goTo.bind(ctx, link.pathname);
-        link.href = 'javascript:void(null);';
-      });
+        link.onclick = _goTo.bind(ctx, link.pathname)
+        link.href = 'javascript:void(null);'
+      })
   }
 
-  var _routerContainer = document.createElement('div');
+  var _routerContainer = document.createElement('div')
 
-  this.routes = routes;
+  this.routes = routes
   this.errorHTML = options.errorHTML
     ? options.errorHTML
-    : '<div>Not Found</div>';
+    : '<div>Not Found</div>'
 
   window.onload = function() {
-    window.router = this;
-    this.container = document.getElementById(container);
+    window.router = this
+    this.container = document.getElementById(container)
     if (options.header) {
-      _appendComponent.call(this, 'header', options.header, this.container);
+      _appendComponent.call(this, 'header', options.header, this.container)
     }
-    this.container.appendChild(_routerContainer);
+    this.container.appendChild(_routerContainer)
     if (options.footer) {
-      _appendComponent.call(this, 'footer', options.footer, this.container);
+      _appendComponent.call(this, 'footer', options.footer, this.container)
     }
-    this.goTo(window.location.pathname);
+    this.goTo(window.location.pathname)
     if (options.debug) {
       console.log(
         '%cRouter Initialized with Routes: ' +
           Object.keys(this.routes).join(', '),
-        'color: blue; font-size: 14px;',
-      );
+        'color: blue; font-size: 14px;'
+      )
       console.log(
         '%cCurrent Path: ' + window.location.pathname,
-        'color: orange; font-size: 14px;',
-      );
+        'color: orange; font-size: 14px;'
+      )
     }
-    _replaceLinks.call(this, this, document);
-  }.call(this);
+    _replaceLinks.call(this, this, document)
+  }.call(this)
 }
 
-module.exports = Router;
+module.exports = Router
