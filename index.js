08a823ec3a8284bebb20cdee8ba74a8c44991720
David Alexander Leigh <daleighan@gmail.com>
2019-10-12 21:55:49 -0700

    Some cleaning up and reorganization of logic

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
-function Router(container, routes, errorHTML) {
+function Router(container, routes, debug, errorHTML) {
   this.router = document.createElement('div');
   this.routes = routes;
-  console.log(
-    'Router Initialized with Routes: ',
-    Object.keys(this.routes).join(', '),
-  );
-  console.log('Current Path: ', window.location.pathname);
-  document.getElementById(container).appendChild(this.router);
-  if (this.routes[window.location.pathname]) {
-    this.router.innerHTML = this.routes[window.location.pathname];
-  } else {
-    this.router.innerHTML = errorHTML ? erroHTML : '<div>Not Found</div>';
-  }
+
   this.goTo = function(route) {
-    window.history.pushState({}, route, `${window.location.origin}/${route}/`);
+    window.history.pushState({}, route, `${window.location.origin}${route}`);
     this.router.innerHTML = routes[window.location.pathname];
     if (this.routes[window.location.pathname]) {
       this.router.innerHTML = this.routes[window.location.pathname];
+      if (debug) {
+        console.log(`Navigated to: ${route}`);
+      }
     } else {
       this.router.innerHTML = errorHTML ? erroHTML : '<div>Not Found</div>';
+      if (debug) {
+        console.error(`Route not found: ${route}`);
+      }
     }
   };
+
   window.router = this;
+
+  window.onload = () => {
+    document.getElementById(container).appendChild(this.router);
+    this.goTo(window.location.pathname);
+    if (debug) {
+      console.log(
+        'Router Initialized with Routes: ',
+        Object.keys(this.routes).join(', '),
+      );
+      console.log('Current Path: ', window.location.pathname);
+    }
+  };
 }
 
 module.exports = Router;
